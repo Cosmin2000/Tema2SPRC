d8655c851f363e728d396d0c47151913f72f62a6
Cosmin Grigore <57139926+Cosmin2000@users.noreply.github.com>
2022-12-08 00:20:30 +0200

    API terminat, fara refactorizare

--- services/server/tema.py
+++ services/server/tema.py
@@ -35,10 +35,13 @@ def process_get_response(fields, table_data):
 	for num, row in enumerate(table_data):
 		# am facut in cast fiindca imi transporta datele lon si lat de tip string
 		resultRow = {}
-		for i in range(len(fields) - 2):
+		# for i in range(len(fields) - 2):
+		# 	resultRow[fields[i]] = row[i]
+		# resultRow[fields[len(fields) - 2]] = float(row[len(fields) - 2])
+		# resultRow[fields[len(fields) - 1]] = float(row[len(fields) - 1])
+		# result.append(resultRow)
+		for i in range(len(fields)):
 			resultRow[fields[i]] = row[i]
-		resultRow[fields[len(fields) - 2]] = float(row[len(fields) - 2])
-		resultRow[fields[len(fields) - 1]] = float(row[len(fields) - 1])
 		result.append(resultRow)
 	return result
 
@@ -62,7 +65,8 @@ def add_country():
 	response = {'id':returned_id}
 	return Response(
 		response=json.dumps(response),
-		status=201
+		status=201,
+		mimetype='application/json' 
 	)
 
 @server.route("/api/countries", methods=["GET"])
@@ -70,7 +74,8 @@ def get_countries():
 	data = request.json
 
 	try:
-		curr.execute('SELECT * FROM Tari')
+		curr.execute('SELECT id, nume_tara, latitudine::float, longitudine::float FROM Tari')
+		# print([i[0] for i in curr.description], flush=True)
 		table_data = curr.fetchall()
 		fields = ['id','nume', 'lat', 'lon']
 		
@@ -83,6 +88,7 @@ def get_countries():
 	return Response(
 		response=json.dumps(result, sort_keys=False),
 		status=200,
+		mimetype='application/json' 
 	)
 
 @server.route("/api/countries/<id>", methods=["PUT"])
@@ -151,7 +157,8 @@ def add_city():
 	response = {'id':returned_id}
 	return Response(
 		response=json.dumps(response),
-		status=201
+		status=201,
+		mimetype='application/json' 
 	)
 
 @server.route("/api/cities", methods=["GET"])
@@ -159,7 +166,7 @@ def get_cities():
 	data = request.json
 
 	try:
-		curr.execute('SELECT id, id_tara, nume_oras, latitudine, longitudine FROM Orase')
+		curr.execute('SELECT id, id_tara, nume_oras, latitudine::float, longitudine::float FROM Orase')
 		table_data = curr.fetchall()
 		fields = ['id', 'idTara','nume', 'lat', 'lon']
 
@@ -172,6 +179,7 @@ def get_cities():
 	return Response(
 		response=json.dumps(result, sort_keys=False),
 		status=200,
+		mimetype='application/json' 
 	)
 
 @server.route("/api/cities/country/<id_Tara>", methods=["GET"])
@@ -182,7 +190,7 @@ def get_cities_by_country(id_Tara=None):
 		return Response(status=400)
 
 	try:
-		curr.execute('SELECT * FROM Orase WHERE id_tara = %s',(id_Tara))
+		curr.execute('SELECT id, id_tara, nume_oras, latitudine::float , longitudine::float FROM Orase WHERE id_tara = %s',(id_Tara))
 		table_data = curr.fetchall()
 		fields = ['id', 'idTara','nume', 'lat', 'lon']
 		result = process_get_response(fields, table_data)
@@ -190,6 +198,7 @@ def get_cities_by_country(id_Tara=None):
 		return Response(status=500)
 
 	return Response(status=200,
+					mimetype='application/json', 
 					response=json.dumps(result, sort_keys=False))
 
 @server.route("/api/cities/<id>", methods=["PUT"])
@@ -215,6 +224,9 @@ def modify_city(id=None):
 		# unique violation
 		return Response(
 			status=409)
+	except psycopg2.errors.lookup("23503"):
+		# foreign key
+		return Response(status=409)
 
 	return Response(
 		status=200
@@ -237,6 +249,313 @@ def delete_city(id=None):
 		return Response(
 			status=200
 		)
+
+
+# ================================== TEMPERATURES ==============================
+
+
+@server.route("/api/temperatures", methods=["POST"])
+def add_temperature():
+	data = request.json
+	types_ref = [['int'], ['int', 'float']]
+
+	if not valid_body(types_ref, request):
+		return Response(status=400)
+
+	try:
+		curr.execute('INSERT INTO Temperaturi(id_oras, valoare) VALUES (%s, %s) RETURNING id',  ( data['idOras'],data['valoare']))
+		returned_id = curr.fetchone()[0]
+	except psycopg2.Error as e: 
+		print(e)
+		return Response(
+			status=409)
+
+	response = {'id':returned_id}
+	return Response(
+		response=json.dumps(response),
+		status=201,
+		mimetype='application/json' 
+	)
+
+@server.route("/api/temperatures", methods=["GET"])
+def get_temperatures():
+	data = request.json
+	args = request.args
+	lat = args.get('lat', type=float)
+	lon = args.get('lon')
+	fromDate = args.get('from')
+	toDate = args.get('until')
+	params = []
+	conditions = []
+	if lat:
+		conditions.append('o.latitudine=%s')
+		params.append(lat)
+	if lon:
+		conditions.append('o.longitudine=%s')
+		params.append(lon)
+	if fromDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') >= %s')
+		params.append(fromDate)
+	if toDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') <= %s')
+		params.append(toDate)
+	condition = '' if len(conditions) == 0 else ' WHERE ' + ' AND '.join(conditions)
+	# print(condition, flush=True)
+	try:
+		statement = ''' SELECT t.id, t.valoare::float, TO_CHAR(t.timestamp, 'YYYY-MM-DD'), t.id_oras
+					FROM Temperaturi t 
+					INNER JOIN Orase o ON o.id=t.id_oras 
+					%s ''' % condition
+		curr.execute(statement, params)
+
+		table_data = curr.fetchall()
+		# print(table_data, flush=True)
+		fields = ['id', 'valoare','timestamp']
+		result = []
+		for num, row in enumerate(table_data):
+			# am facut in cast fiindca imi transporta datele lon si lat de tip string
+			resultRow = {}
+			for i in range(len(fields)):
+				resultRow[fields[i]] = row[i]
+			
+			result.append(resultRow)
+	except psycopg2.Error: 
+		# o eroare in caz ca s-a intamplat ceva in baza
+		return Response(
+			status=404)
+
+	return Response(
+		response=json.dumps(result, sort_keys=False),
+		status=200,
+		mimetype='application/json' 
+	)
+
+
+@server.route("/api/temperatures/cities/<id_oras>", methods=["GET"])
+def get_temperatures_by_city(id_oras=None):
+	data = request.json
+	args = request.args
+	lat = args.get('lat', type=float)
+	lon = args.get('lon')
+	fromDate = args.get('from')
+	toDate = args.get('until')
+	params = []
+	conditions = []
+	if lat:
+		conditions.append('o.latitudine=%s')
+		params.append(lat)
+	if lon:
+		conditions.append('o.longitudine=%s')
+		params.append(lon)
+	if fromDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') >= %s')
+		params.append(fromDate)
+	if toDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') <= %s')
+		params.append(toDate)
+	condition = '' if len(conditions) == 0 else ' WHERE ' + ' AND '.join(conditions)
+	# print(condition, flush=True)
+	try:
+		statement = ''' SELECT t.id, t.valoare::float, TO_CHAR(t.timestamp, 'YYYY-MM-DD'), t.id_oras
+					FROM Temperaturi t 
+					INNER JOIN Orase o ON o.id=t.id_oras AND o.id=%s
+					%s ''' % (id_oras, condition)
+		curr.execute(statement, params)
+
+		table_data = curr.fetchall()
+		# print(table_data, flush=True)
+		fields = ['id', 'valoare','timestamp']
+		result = []
+		for num, row in enumerate(table_data):
+			# am facut in cast fiindca imi transporta datele lon si lat de tip string
+			resultRow = {}
+			for i in range(len(fields)):
+				resultRow[fields[i]] = row[i]
+			
+			result.append(resultRow)
+	except psycopg2.Error: 
+		# o eroare in caz ca s-a intamplat ceva in baza
+		return Response(
+			status=404)
+
+	return Response(
+		response=json.dumps(result, sort_keys=False),
+		status=200,
+		mimetype='application/json' 
+	)
+
+@server.route("/api/temperatures/countries/<id_tara>", methods=["GET"])
+def get_temperatures_by_country(id_tara=None):
+	data = request.json
+	args = request.args
+	lat = args.get('lat')
+	lon = args.get('lon')
+	fromDate = args.get('from')
+	toDate = args.get('until')
+	params = []
+	conditions = []
+	if lat:
+		conditions.append('o.latitudine=%s')
+		params.append(lat)
+	if lon:
+		conditions.append('o.longitudine=%s')
+		params.append(lon)
+	if fromDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') >= %s')
+		params.append(fromDate)
+	if toDate:
+		conditions.append('TO_CHAR(t.timestamp, \'YYYY-MM-DD\') <= %s')
+		params.append(toDate)
+	condition = '' if len(conditions) == 0 else ' WHERE ' + ' AND '.join(conditions)
+	# print(condition, flush=True)
+	try:
+		statement = ''' SELECT t.id, t.valoare::float, TO_CHAR(t.timestamp, 'YYYY-MM-DD'), t.id_oras
+					FROM Temperaturi t 
+					INNER JOIN Orase o ON o.id=t.id_oras AND o.id_tara=%s
+					%s ''' % (id_tara, condition)
+		curr.execute(statement, params)
+
+		table_data = curr.fetchall()
+		# print(table_data, flush=True)
+		fields = ['id', 'valoare','timestamp']
+		result = []
+		for num, row in enumerate(table_data):
+			# am facut in cast fiindca imi transporta datele lon si lat de tip string
+			resultRow = {}
+			for i in range(len(fields)):
+				resultRow[fields[i]] = row[i]
+			
+			result.append(resultRow)
+	except psycopg2.Error: 
+		# o eroare in caz ca s-a intamplat ceva in baza
+		return Response(
+			status=404)
+
+	return Response(
+		response=json.dumps(result, sort_keys=False),
+		status=200,
+		mimetype='application/json' 
+	)
+
+@server.route("/api/temperatures/<id>", methods=["PUT"])
+def modify_temperature(id=None):
+	data = request.json
+
+	print(request.json, flush=True)
+	print(id, flush=True)
+	types_ref = [['int'],['int'], ['int' ,'float'],]
+	# verific daca argumentele au tipul potrivit
+	if (id == None or not valid_body(types_ref, request) or int(id) != data['id']):
+		return Response(status=400)
+
+	try:
+		curr.execute('UPDATE Temperaturi SET id_oras = %s, valoare = %s WHERE id = %s',
+		(data['idOras'] ,data['valoare'], id))
+
+	except psycopg2.errors.lookup("20000"): 
+		# nu s-a gasit temperatura cu id-ul specificat
+		return Response(
+			status=404)
+	except psycopg2.errors.lookup("23505"): 
+		# unique violation
+		return Response(
+			status=409)
+	except:
+		return Response(status=400)
+
+	return Response(
+		status=200
+	)
+
+@server.route("/api/temperatures/<id>", methods=["DELETE"])
+def delete_temperatures(id=None):
+	data = request.json
+
+	if id == None:
+		return Response(status = 400)
+
+	curr.execute('DELETE FROM Temperaturi WHERE id = %s', (id,))
+	if (curr.rowcount == 0):
+		return Response(
+			status=404
+		)
+	else:
+		return Response(
+			status=200
+		)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # @server.route("/movie/<id>", methods=["GET", "PUT", "DELETE"])
 # def handle_request(id=None):
